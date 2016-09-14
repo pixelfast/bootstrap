4d2e3ef4fd954442d13238b2420ec8d490f88351
Michael Wright <michael@pixelfast.com>
2016-09-13 23:28:46 -0700

    Modified index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-echo "hi";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Theme Simply Me</title>
+  <meta charset="utf-8">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+	<?php
+	echo "<h1>My Bootstrap Themes</h1>";
+	echo "<p>From W3Schools.com</p>"
+	?>
+</body>
+</html>
